72265c684f943d546973f854867ba89448aa6c73
Tomáš Zajíček <tomas.zajicek@promethist.ai>
2019-07-31 15:01:51 +0200

    common classes moved to common-lib

--- services/port/app/src/main/kotlin/com/promethistai/common/resources/Check.kt
+++
@@ -1,3 +0,0 @@
-package com.promethistai.common.resources
-
-data class Check(val health: Double, val name: String?, val namespace: String?, val git_ref: String?, val git_commit: String?, val app_image: String?)
