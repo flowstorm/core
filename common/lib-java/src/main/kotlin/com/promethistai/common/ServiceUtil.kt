be5a127701b98e6113093e30c4343beef7caeb20
Tomáš Zajíček <tomas.zajicek@promethist.ai>
2019-10-06 11:59:38 +0200

    service endpoint url helper funcionality

---
+++ common/lib-java/src/main/kotlin/com/promethistai/common/ServiceUtil.kt
@@ -0,0 +1,30 @@
+package com.promethistai.common
+
+object ServiceUtil {
+
+    val servicePorts = mapOf<String, Int>(
+            "port" to 8080,
+            "brainquist" to 8089,
+            "brainquist-server" to 8089, //TODO remove - just for backward compatibility
+            "illusionist" to 8090,
+            "helena" to 8091,
+            "editor" to 8092
+    )
+
+    enum class RunMode { local, docker, dist, detect }
+
+    fun getEndpointUrl(serviceName: String, runMode: RunMode = RunMode.detect): String =
+        when (runMode) {
+            RunMode.local -> "http://localhost:${servicePorts[serviceName]}"
+            RunMode.docker -> "http://${serviceName}:8080"
+            RunMode.dist -> "https://${serviceName}" +
+                    (if (AppConfig.instance["namespace"] != "default")
+                        "." + AppConfig.instance["namespace"]
+                    else
+                        "") + ".promethist.ai"
+            RunMode.detect ->
+                getEndpointUrl(serviceName,RunMode.valueOf(
+                    System.getenv("RUN_MODE")?:AppConfig.instance.get("runmode", "local")))
+        }
+
+}
