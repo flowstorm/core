b32789f2b8a33086b6e1aac8842d45ceb5d047aa
Tomáš Zajíček <tomas.zajicek@promethist.ai>
2019-11-12 21:11:12 +0100

    logging messages to slack

--- services/port/README.md
+++ services/port/README.md
@@ -7,7 +7,7 @@ mvn jetty:run-war
 curl -i "localhost:8080/contract?key=VeraTest.4"
 
 # send message
-curl -i -X PUT -H "Content-Type: application/json" -d '{"items":{"text":"hello"}}' "localhost:8080/message?key=test1"
+curl -i -X PUT -H "Content-Type: application/json" -d '{"items":[{"text":"hello"}]}' "localhost:8080/message?key=test1"
 
 # read file
 curl -i "localhost:8080/file/5d5cd56ac87aa5439369e863"
---
+++ services/port/app/src/main/kotlin/com/promethistai/port/SlackService.kt
@@ -0,0 +1,53 @@
+package com.promethistai.port
+
+import com.promethistai.common.AppConfig
+import com.promethistai.common.ObjectUtil
+import com.promethistai.port.model.Message
+import java.io.DataOutputStream
+import java.net.HttpURLConnection
+import java.net.URL
+import javax.inject.Inject
+import java.io.IOException
+import jdk.nashorn.internal.runtime.ScriptingFunctions.readLine
+import java.io.InputStreamReader
+import java.io.BufferedReader
+
+
+
+class SlackService {
+
+    data class Post(val text: String)
+
+    @Inject
+    lateinit var config: AppConfig
+
+    fun sendMessage(message: Message) {
+        val str = StringBuilder("[${config["namespace"]}] <${message.sender}>:")
+        message.items.forEach {
+            str.append(' ').append(it.text)
+        }
+        val post = Post(str.toString())
+
+        val url = URL(config["slack.url"])
+        try {
+            val con = url.openConnection() as HttpURLConnection
+            con.setRequestMethod("POST")
+            con.setRequestProperty("Content-Type", "application/json; charset=utf-8")
+            con.setDoOutput(true)
+            val json = ObjectUtil.defaultMapper.writeValueAsString(post)
+            con.outputStream.use {
+                it.write(json.toByteArray(Charsets.UTF_8))
+                it.flush()
+            }
+            val buf = StringBuilder()
+
+            BufferedReader(InputStreamReader(con.inputStream)).readLines().forEach {
+                buf.append(it)
+            }
+            println(json)
+            println(buf)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+}
