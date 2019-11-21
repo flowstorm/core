dd0db75eac73743423265017bf21a29fd84cfead
Tomáš Zajíček <tomas.zajicek@promethist.ai>
2019-11-21 17:48:20 +0100

    support for disabling slack logging

--- services/port/app/src/main/kotlin/com/promethistai/port/SlackService.kt
+++ services/port/app/src/main/kotlin/com/promethistai/port/SlackService.kt
@@ -17,32 +17,34 @@ class SlackService {
     lateinit var config: AppConfig
 
     fun sendMessage(message: Message) {
-        val str = StringBuilder("[${config["namespace"]}] <${message.sender}>:")
-        message.items.forEach {
-            str.append(' ').append(it.text)
-        }
-        val post = Post(str.toString())
-
-        val url = URL(config["slack.url"])
-        try {
-            val con = url.openConnection() as HttpURLConnection
-            con.setRequestMethod("POST")
-            con.setRequestProperty("Content-Type", "application/json; charset=utf-8")
-            con.setDoOutput(true)
-            val json = ObjectUtil.defaultMapper.writeValueAsString(post)
-            con.outputStream.use {
-                it.write(json.toByteArray(Charsets.UTF_8))
-                it.flush()
+        if ("true" == config["slack.enable"]) {
+            val str = StringBuilder("[${config["namespace"]}] <${message.sender}>:")
+            message.items.forEach {
+                str.append(' ').append(it.text)
             }
-            val buf = StringBuilder()
+            val post = Post(str.toString())
 
-            BufferedReader(InputStreamReader(con.inputStream)).readLines().forEach {
-                buf.append(it)
+            val url = URL(config["slack.url"])
+            try {
+                val con = url.openConnection() as HttpURLConnection
+                con.setRequestMethod("POST")
+                con.setRequestProperty("Content-Type", "application/json; charset=utf-8")
+                con.setDoOutput(true)
+                val json = ObjectUtil.defaultMapper.writeValueAsString(post)
+                con.outputStream.use {
+                    it.write(json.toByteArray(Charsets.UTF_8))
+                    it.flush()
+                }
+                val buf = StringBuilder()
+
+                BufferedReader(InputStreamReader(con.inputStream)).readLines().forEach {
+                    buf.append(it)
+                }
+                println(json)
+                println(buf)
+            } catch (e: Exception) {
+                e.printStackTrace()
             }
-            println(json)
-            println(buf)
-        } catch (e: Exception) {
-            e.printStackTrace()
         }
     }
 }
