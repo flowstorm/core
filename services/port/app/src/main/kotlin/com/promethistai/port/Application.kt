9db53787470224b6c29268ade933f3651a5f7e51
Tomáš Zajíček <tomas.zajicek@promethist.ai>
2019-11-23 14:50:06 +0100

    FIX missing slack service

--- services/port/app/src/main/kotlin/com/promethistai/port/Application.kt
+++ services/port/app/src/main/kotlin/com/promethistai/port/Application.kt
@@ -20,6 +20,7 @@ class Application : JerseyApplication() {
                 bindTo(MongoDatabase::class.java, KMongo.createClient(ConnectionString(AppConfig.instance["database.url"])).getDatabase(AppConfig.instance["database.name"]))
                 // services
                 bindTo(DataService::class.java)
+                bindTo(SlackService::class.java)
                 bindTo(BotService::class.java, BotSelectorService::class.java)
                 bindTo(BotRemoteService::class.java)
                 bindTo(EchoService::class.java)
