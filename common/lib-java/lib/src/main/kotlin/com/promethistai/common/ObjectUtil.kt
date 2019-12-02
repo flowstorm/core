0e1fc1728306714913fc8cd1a7cd07bfa3cad843
Tomas Kormanak <tomas.kormanak@kormik.net>
2019-12-02 10:12:48 +0100

    Fix model serialization.

--- common/lib-java/lib/src/main/kotlin/com/promethistai/common/ObjectUtil.kt
+++ common/lib-java/lib/src/main/kotlin/com/promethistai/common/ObjectUtil.kt
@@ -3,6 +3,8 @@ package com.promethistai.common
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.litote.kmongo.id.jackson.IdJacksonModule
 import java.text.SimpleDateFormat
 
 object ObjectUtil {
@@ -10,6 +12,7 @@ object ObjectUtil {
     @JvmStatic
     val defaultMapper = ObjectMapper()
             .registerModule(KotlinModule())
+            .registerModule(IdJacksonModule())
             .setDateFormat(SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ"))
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 }
