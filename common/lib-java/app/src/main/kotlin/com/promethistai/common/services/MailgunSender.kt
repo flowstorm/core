1770a097500a1f868af2ecae0d925027a20051c3
Tomas Kormanak <tomas.kormanak@kormik.net>
2019-12-19 14:10:26 +0100

    AIS-37: Add email sender and mailgun implementation.

---
+++ common/lib-java/app/src/main/kotlin/com/promethistai/common/services/MailgunSender.kt
@@ -0,0 +1,22 @@
+package com.promethistai.common.services
+
+import com.commit451.mailgun.Contact
+import com.commit451.mailgun.Mailgun
+import com.commit451.mailgun.SendMessageRequest
+
+class MailgunSender(val mailgun: Mailgun, val from: Contact) : EmailSender {
+
+    override fun sendEmail(recipient: EmailSender.Recipient, subject: String, templateName: String, templateVariables: Map<String, String>) {
+
+        val to = mutableListOf(Contact(recipient.email, recipient.name))
+
+        val requestBuilder = SendMessageRequest.Builder(from)
+                .to(to)
+                .subject(subject)
+                .template(templateName)
+                .templateVariables(templateVariables)
+                .text("") //we send empty text body when using a template
+
+        val response = mailgun.sendMessage(requestBuilder.build()).blockingGet()
+    }
+}
