b8e96e6be51023f17d9da0bddd800240c3a153cf
Tomáš Zajíček <tomas.zajicek@promethist.ai>
2020-03-02 08:52:53 +0100

    ServiceUtil moved from common-lib to core-api

---
+++ services/core/api/src/main/kotlin/com/promethistai/core/ServiceUtil.kt
@@ -0,0 +1,34 @@
+package com.promethistai.core
+
+import com.promethistai.common.AppConfig
+
+object ServiceUtil {
+
+    val servicePorts = mapOf(
+            "port" to 8080,
+            "filestore" to 8082,
+            "core" to 8088,
+            "admin" to 8089,
+            "illusionist" to 8090,
+            "helena" to 8091,
+            "editor" to 8092,
+            "cassandra" to 8093
+    )
+
+    enum class RunMode { local, docker, dist, detect }
+
+    fun getEndpointUrl(serviceName: String, runMode: RunMode = RunMode.detect): String =
+            when (runMode) {
+                RunMode.local -> "http://localhost:${servicePorts[serviceName]}"
+                RunMode.docker -> "http://${serviceName}:8080"
+                RunMode.dist -> "https://${serviceName}" +
+                        (if (AppConfig.instance["namespace"] != "default")
+                            "." + AppConfig.instance["namespace"]
+                        else
+                            "") + ".promethistai.com"
+                RunMode.detect ->
+                    getEndpointUrl(serviceName, RunMode.valueOf(
+                            System.getenv("RUN_MODE") ?: AppConfig.instance.get("runmode", "dist")))
+            }
+
+}
