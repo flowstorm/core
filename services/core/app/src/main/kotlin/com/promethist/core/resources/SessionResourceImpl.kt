1471112e15a5e0407b00e660e12c455f92888f5d
Tomas Kormanak <tomas.kormanak@kormik.net>
2020-03-13 12:04:09 +0100

    AIS-92: Allow to filter session by user and attributes.

--- services/core/app/src/main/kotlin/com/promethist/core/resources/SessionResourceImpl.kt
+++ services/core/app/src/main/kotlin/com/promethist/core/resources/SessionResourceImpl.kt
@@ -1,6 +1,7 @@
 package com.promethist.core.resources
 
 import com.mongodb.client.MongoDatabase
+import com.mongodb.client.model.Filters
 import com.promethist.common.query.MongoFiltersFactory
 import com.promethist.common.query.Query
 import com.promethist.common.query.QueryParams
@@ -9,6 +10,7 @@ import com.promethist.core.model.User
 import org.bson.conversions.Bson
 import org.litote.kmongo.*
 import org.litote.kmongo.id.ObjectIdGenerator
+import org.litote.kmongo.id.WrappedObjectId
 import javax.inject.Inject
 
 class SessionResourceImpl: SessionResource {
@@ -35,9 +37,17 @@ class SessionResourceImpl: SessionResource {
 
             add(sort(descending(Session::datetime, Session::_id)))
             add(match(*MongoFiltersFactory.createFilters(Session::class, query, includeSeek = false).toTypedArray()))
-            add(limit(query.limit))
         }
 
+        query.filters.firstOrNull { it.name == "user._id" && it.operator == Query.Operator.`in` }?.let {
+            pipeline.add(match(Session::user / User::_id `in` it.value.split(",").map { WrappedObjectId<User>(it) }))
+        }
+
+        query.filters.firstOrNull { it.name.startsWith("attributes.") && it.operator == Query.Operator.eq }?.let {
+            pipeline.add(match(Filters.eq(it.name, it.value)))
+        }
+
+        pipeline.add(limit(query.limit))
         val result = database.getCollection<Session>().aggregate(pipeline).toMutableList()
         return result
     }
