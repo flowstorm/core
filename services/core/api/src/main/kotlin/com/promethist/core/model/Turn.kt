7b6045fbdb08611be1bd9a684670c2a1a0c9fd7f
Tomáš Zajíček <tomas.zajicek@promethist.ai>
2020-03-19 08:34:40 +0100

    input classes

--- services/core/api/src/main/kotlin/com/promethist/core/model/Turn.kt
+++ services/core/api/src/main/kotlin/com/promethist/core/model/Turn.kt
@@ -2,6 +2,7 @@ package com.promethist.core.model
 
 import com.fasterxml.jackson.annotation.JsonSubTypes
 import com.fasterxml.jackson.annotation.JsonTypeInfo
+import com.promethist.common.ObjectUtil
 import com.promethist.core.type.Dynamic
 import java.util.*
 
@@ -13,7 +14,11 @@ data class Turn(
 ) {
     data class DialogueStackFrame(val name: String, var nodeId: Int = 0)
 
-    data class Input(val message: String, var clazz: String, val tokens: MutableList<Token> = mutableListOf()) {
+    data class Input(val text: String, val classes: MutableList<Class> = mutableListOf(), val tokens: MutableList<Token> = mutableListOf()) {
+
+        data class Class(val type: Type, val name: String) {
+            enum class Type { Intent, Entity }
+        }
 
         @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
         @JsonSubTypes(
@@ -22,8 +27,23 @@ data class Turn(
         )
         open class Token(open val text: String)
 
-        data class Word(override val text: String, val classes: MutableList<String> = mutableListOf()) : Token(text)
+        data class Word(override val text: String, val classes: MutableList<Class> = mutableListOf()) : Token(text)
 
         data class Punctuation(override val text: String) : Token(text)
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val input = Input("I see a dog and a cat.", mutableListOf(Input.Class(Input.Class.Type.Intent, "-1")), mutableListOf(
+                    Input.Word("i"),
+                    Input.Word("see"),
+                    Input.Word("dog", mutableListOf(Input.Class(Input.Class.Type.Entity, "animal"))),
+                    Input.Word("and"),
+                    Input.Word("cat", mutableListOf(Input.Class(Input.Class.Type.Entity, "animal"))),
+                    Input.Punctuation(".")
+            ))
+            println(ObjectUtil.defaultMapper.writeValueAsString(input))
+        }
     }
 }
