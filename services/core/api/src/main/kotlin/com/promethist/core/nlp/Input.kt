c0cad949a6558eccfee6b0a700ea47e09ded26da
Tomáš Zajíček <tomas.zajicek@promethist.ai>
2020-03-23 09:13:57 +0100

    input entities map

--- services/core/api/src/main/kotlin/com/promethist/core/nlp/Input.kt
+++ services/core/api/src/main/kotlin/com/promethist/core/nlp/Input.kt
@@ -19,8 +19,8 @@ data class Input(var text: String, val classes: MutableList<Class> = mutableList
     open class Token(open val text: String)
 
     data class Word(override val text: String, val classes: MutableList<Class> = mutableListOf()) : Token(text) {
-        fun isClass(type: Class.Type, name: String) = classes.any { it.type == type && it.name == name }
-        fun isEntity(name: String) = isClass(Class.Type.Entity, name)
+        fun hasClass(type: Class.Type, name: String) = classes.any { it.type == type && it.name == name }
+        fun isEntity(name: String) = hasClass(Class.Type.Entity, name)
     }
 
     data class Punctuation(override val text: String) : Token(text)
@@ -34,21 +34,46 @@ data class Input(var text: String, val classes: MutableList<Class> = mutableList
     }
 
     val words: WordList by lazy { WordList(tokens.filterIsInstance<Word>()) }
+    val entities: Map<String, List<String>> by lazy {
+        val map = mutableMapOf<String, MutableList<String>>()
+        words.forEach { word ->
+            word.classes.forEach {
+                if (it.type == Class.Type.Entity) {
+                    val beginning = it.name.startsWith("B-")
+                    val inside = it.name.startsWith("I-")
+                    val name = if (beginning || inside) it.name.substring(2) else it.name
+                    if (!map.containsKey(name))
+                        map[name] = mutableListOf()
+                    else if (inside) {
+                        val last = map[name]!!.size - 1
+                        map[name]!![last] = map[name]!![last] + " " + word.text
+                    }
+                    if (!inside)
+                        map[name]!!.add(word.text)
+                }
+            }
+        }
+        map
+    }
 
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            val input = Input("I see a dog and a cat.", mutableListOf(Class(Class.Type.Intent, "-1")),
+            val input = Input("I see a dog, a red rose and a cat.", mutableListOf(Class(Class.Type.Intent, "-1")),
                     mutableListOf(
                             Word("i"),
                             Word("see"),
                             Word("dog", mutableListOf(Class(Class.Type.Entity, "animal"))),
+                            Punctuation(","),
+                            Word("red", mutableListOf(Class(Class.Type.Entity, "B-flower"))),
+                            Word("rose", mutableListOf(Class(Class.Type.Entity, "I-flower"))),
                             Word("and"),
                             Word("cat", mutableListOf(Class(Class.Type.Entity, "animal"))),
                             Punctuation(".")
                     )
             )
             println(input.words.entities("animal"))
+            println(input.entities)
             println(ObjectUtil.defaultMapper.writeValueAsString(input))
         }
     }
