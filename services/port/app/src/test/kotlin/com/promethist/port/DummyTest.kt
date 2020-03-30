85b3b3c2af88210c88749ded27c99502e5339993
Tomas Kormanak <tomas.kormanak@kormik.net>
2020-03-30 12:40:50 +0200

    Setup JUnit tests.

---
+++ services/port/app/src/test/kotlin/com/promethist/port/DummyTest.kt
@@ -0,0 +1,14 @@
+package com.promethist.port
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class DummyTest {
+    //Dummy test to keep test directory in git and avoid warnings in build until we introduce actual tests.
+
+    @Test
+    fun `dummy test case`() {
+        assert(true)
+    }
+}
