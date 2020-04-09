9e0f70f9d273d09656d34f9ec62ec8302ab462cc
Tomas Kormanak <tomas.kormanak@kormik.net>
2020-04-09 11:47:04 +0200

    Core: Fix - we need to copy dialoguestack into new turn, accidentally removed

--- services/core/app/src/main/kotlin/com/promethist/core/context/ContextFactory.kt
+++ services/core/app/src/main/kotlin/com/promethist/core/context/ContextFactory.kt
@@ -8,6 +8,7 @@ import com.promethist.core.model.metrics.Metrics
 import com.promethist.core.profile.ProfileRepository
 import com.promethist.core.runtime.DialogueLog
 import org.slf4j.LoggerFactory
+import java.util.*
 import javax.inject.Inject
 
 class ContextFactory {
@@ -22,11 +23,13 @@ class ContextFactory {
         val profile = profileRepository.find(session.user._id)
                 ?: Profile(user_id = session.user._id, name = session.user.username)
 
+        val stack = session.turns.lastOrNull()?.dialogueStack ?: LinkedList()
+
         return Context(
                 pipeline,
                 profile,
                 session,
-                Turn(input),
+                Turn(input = input, dialogueStack = stack),
                 Metrics(session.metrics),
                 dialogueLog.logger
         )
