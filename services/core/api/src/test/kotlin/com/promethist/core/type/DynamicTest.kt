32a92f5034e97f0b08ca6dc925b7b88dbbc5f0e5
Tomáš Zajíček <tomas.zajicek@promethist.ai>
2020-04-20 12:07:58 +0200

    just code polishing

--- services/core/api/src/test/kotlin/com/promethist/core/type/DynamicTest.kt
+++ services/core/api/src/test/kotlin/com/promethist/core/type/DynamicTest.kt
@@ -15,7 +15,7 @@ class DynamicTest {
         assertEquals(10, mem("counter"))
     }
 
-    @Test @Disabled("Should work, bug in Dynamic?")
+    @Test
     fun `assign nested value`() {
         val mem = Dynamic()
         mem("counter.nested", 10)
@@ -29,7 +29,7 @@ class DynamicTest {
         assertEquals(10, mem("counter"))
     }
 
-    @Test @Disabled("Should work, bug in Dynamic?")
+    @Test
     fun `assign nested value using lambda`() {
         val mem = Dynamic()
         mem<Int>("counter.nested") { value = 10; Unit }
@@ -41,54 +41,5 @@ class DynamicTest {
         val mem = Dynamic()
         val result = mem<Int>("counter") { value = 10; "result" }
         assertEquals("result", result)
-    }
-
-    @Test
-    fun `dynamic test case`() {
-        val mem = Dynamic()
-        mem("counter.a", 1) // direct assignment (no need to declare type - taken from input value)
-        mem<Int>("counter.a") { ++value } // lambda assignment - note: must always return any value:
-        mem<Int>("counter.a") {
-            value += 1
-            Unit
-        }
-        mem<Int>("counter.b") { ++value }
-        mem<Dynamic>("counter") {
-            value<Int>("a") {
-                println("a=$value")
-            }
-            value<Int>("b") {
-                println("b=$value")
-            }
-        }
-
-        if (mem<Int>("level") { apply { value = if (value == 0) 1 else 2 }.value } == 1)
-
-        mem("user.name", "John") // another direct assignment
-        val node = mem<String>("user.name") {
-            value = "Jerry"
-            -5
-        }
-        println(node)
-
-        mem.set<Int>("col.set") { value.add(1) }
-        mem.set<Int>("col.set") { value.add(2) }
-        mem.list<Int>("col.list") { value.add(3) }
-        mem.list<Int>("col.list") { value.add(4) }
-
-        println(mem("col"))
-        println(mem("counter.a") == 2)
-        println(mem("user.name") == "Jerry")
-        println(mem.set<Int>("col.set").contains(1))
-
-        // list of time memories
-        mem.set<TimeString>("animals") {
-            value.add(TimeString("dog"))
-            value.add(TimeString("cat"))
-            value.add(TimeString("dog"))
-        }
-        mem.set<TimeString>("animals").find { it.value == "cat" }?.let {
-            println("cat remembered ${it.time}")
-        }
     }
 }
