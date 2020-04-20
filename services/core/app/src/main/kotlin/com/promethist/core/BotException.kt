895c63dd6f5490ff38547ca529be8121e6a13900
Tomas Kormanak <tomas.kormanak@kormik.net>
2020-04-20 22:12:52 +0200

    Remove unused class.

--- services/core/app/src/main/kotlin/com/promethist/core/BotException.kt
+++
@@ -1,9 +0,0 @@
-package com.promethist.core
-
-class BotException(var type: Type, var id: String, override var message: String? = null) : Exception(message) {
-    enum class Type {
-        NO_VALID_USER,
-        NO_VALID_DEVICE,
-        NO_APPLICATIONS
-    }
-}
