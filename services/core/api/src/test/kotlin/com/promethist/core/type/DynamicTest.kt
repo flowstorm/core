25fbe750f15c5c50250501c0937dffb8230116ab
Tomas Kormanak <tomas.kormanak@kormik.net>
2020-04-20 10:32:42 +0200

    Unit test for Dynamic

--- services/core/api/src/test/kotlin/com/promethist/core/type/DynamicTest.kt
+++ services/core/api/src/test/kotlin/com/promethist/core/type/DynamicTest.kt
@@ -1,10 +1,47 @@
 package com.promethist.core.type
 
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class DynamicTest {
+
+    @Test
+    fun `assign value`() {
+        val mem = Dynamic()
+        mem("counter", 10)
+        assertEquals(10, mem("counter"))
+    }
+
+    @Test @Disabled("Should work, bug in Dynamic?")
+    fun `assign nested value`() {
+        val mem = Dynamic()
+        mem("counter.nested", 10)
+        assertEquals(10, mem("counter.nested"))
+    }
+
+    @Test
+    fun `assign value using lambda`() {
+        val mem = Dynamic()
+        mem<Int>("counter") { value = 10; Unit }
+        assertEquals(10, mem("counter"))
+    }
+
+    @Test @Disabled("Should work, bug in Dynamic?")
+    fun `assign nested value using lambda`() {
+        val mem = Dynamic()
+        mem<Int>("counter.nested") { value = 10; Unit }
+        assertEquals(10, mem("counter.nested"))
+    }
+
+    @Test
+    fun `return value comes from lambda`() {
+        val mem = Dynamic()
+        val result = mem<Int>("counter") { value = 10; "result" }
+        assertEquals("result", result)
+    }
 
     @Test
     fun `dynamic test case`() {
