6ee8ad884ab69497453064822e04969227da0f68
Tomáš Zajíček <tomas.zajicek@promethist.ai>
2020-05-26 19:14:10 +0200

    FIX value/memory collections zero-arg constructors

--- services/core/api/src/main/kotlin/com/promethist/core/type/Collections.kt
+++ services/core/api/src/main/kotlin/com/promethist/core/type/Collections.kt
@@ -11,85 +11,105 @@ open class ValueList<V : Any>(values: Collection<V>) : ArrayList<V>(values), Val
 open class ValueSet<V : Any>(values: Collection<V>) : HashSet<V>(values), ValueCollection
 
 class BooleanMutableList(values: Collection<Boolean>) : ValueList<Boolean>(values) {
+    constructor() : this(emptyList())
     constructor(vararg values: Boolean) : this(values.asList())
 }
 
 class StringMutableList(values: Collection<String>) : ValueList<String>(values) {
+    constructor() : this(emptyList())
     constructor(vararg values: String) : this(values.asList())
 }
 
 class IntMutableList(values: Collection<Int>) : ValueList<Int>(values) {
+    constructor() : this(emptyList())
     constructor(vararg values: Int) : this(values.asList())
 }
 
 class LongMutableList(values: Collection<Long>) : ValueList<Long>(values) {
+    constructor() : this(emptyList())
     constructor(vararg values: Long) : this(values.asList())
 }
 
 class FloatMutableList(values: Collection<Float>) : ValueList<Float>(values) {
+    constructor() : this(emptyList())
     constructor(vararg values: Float) : this(values.asList())
 }
 
 class DoubleMutableList(values: Collection<Double>) : ValueList<Double>(values) {
+    constructor() : this(emptyList())
     constructor(vararg values: Double) : this(values.asList())
 }
 
 class BigDecimalMutableList(values: Collection<BigDecimal>) : ValueList<BigDecimal>(values) {
+    constructor() : this(emptyList())
     constructor(vararg values: BigDecimal) : this(values.asList())
 }
 
 class DateTimeMutableList(values: Collection<DateTime>) : ValueList<DateTime>(values) {
+    constructor() : this(emptyList())
     constructor(vararg values: DateTime) : this(values.asList())
 }
 
 class LocationMutableList(values: Collection<Location>) : ValueList<Location>(values) {
+    constructor() : this(emptyList())
     constructor(vararg values: Location) : this(values.asList())
 }
 
 class BooleanMutableSet(values: Collection<Boolean>) : ValueSet<Boolean>(values) {
+    constructor() : this(emptyList())
     constructor(vararg values: Boolean) : this(values.asList())
 }
 
 class StringMutableSet(values: Collection<String>) : ValueSet<String>(values) {
+    constructor() : this(emptySet())
     constructor(vararg values: String) : this(values.asList())
 }
 
 class IntMutableSet(values: Collection<Int>) : ValueSet<Int>(values) {
+    constructor() : this(emptySet())
     constructor(vararg values: Int) : this(values.asList())
 }
 
 class LongMutableSet(values: Collection<Long>) : ValueSet<Long>(values) {
+    constructor() : this(emptySet())
     constructor(vararg values: Long) : this(values.asList())
 }
 
 class FloatMutableSet(values: Collection<Float>) : ValueSet<Float>(values) {
+    constructor() : this(emptySet())
     constructor(vararg values: Float) : this(values.asList())
 }
 
 class DoubleMutableSet(values: Collection<Double>) : ValueSet<Double>(values) {
+    constructor() : this(emptySet())
     constructor(vararg values: Double) : this(values.asList())
 }
 
 class BigDecimalMutableSet(values: Collection<BigDecimal>) : ValueSet<BigDecimal>(values) {
+    constructor() : this(emptySet())
     constructor(vararg values: BigDecimal) : this(values.asList())
 }
 
 class DateTimeMutableSet(values: Collection<DateTime>) : ValueSet<DateTime>(values) {
+    constructor() : this(emptySet())
     constructor(vararg values: DateTime) : this(values.asList())
 }
 
 class LocationMutableSet(values: Collection<Location>) : ValueSet<Location>(values) {
+    constructor() : this(emptySet())
     constructor(vararg values: Location) : this(values.asList())
 }
 
 interface MemoryCollection<V : Any> : MutableCollection<Memory<V>>, Memorable
 
 class MemoryMutableList<V : Any>(memories: Collection<Memory<V>>) : ArrayList<Memory<V>>(memories), MemoryCollection<V> {
+    constructor() : this(emptyList())
     constructor(vararg memories: Memory<V>) : this(memories.asList())
 }
 
 class MemoryMutableSet<V : Any>(memories: Collection<Memory<V>>) : HashSet<Memory<V>>(memories), MemoryCollection<V> {
 
+    constructor() : this(emptySet())
     constructor(vararg memories: Memory<V>) : this(memories.asList())
 
     override fun add(memory: Memory<V>): Boolean {
