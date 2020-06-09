4b120697e0a934e19a69bf4aff6574e638627c12
konrajak <konradjakub@gmail.com>
2020-06-09 12:03:20 +0200

    FIX negative numbers, decimal numbers without spaces

--- services/core/api/src/main/kotlin/com/promethist/core/Input.kt
+++ services/core/api/src/main/kotlin/com/promethist/core/Input.kt
@@ -62,7 +62,7 @@ data class Input(
     @get:JsonIgnore
     val numbers: List<Number> get() {
         val numbers = mutableListOf<Number>()
-        transcript.text.replace(Regex("([\\d\\.,]+)")) {
+        transcript.text.replace(Regex("(-?[\\d]+(\\.?,?\\d+)?)")) {
             val s = it.groupValues[1].replace(',', '.')
             numbers.add(if (s.indexOf('.') >= 0) s.toFloat() else s.toInt())
             s
