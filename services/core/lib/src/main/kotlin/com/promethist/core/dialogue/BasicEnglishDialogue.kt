8f113d60e3bfbf7ded16ba4bbb62f02eec5ace6c
konrajak <konradjakub@gmail.com>
2020-06-25 12:55:33 +0200

    English global microdialogues

--- services/core/lib/src/main/kotlin/com/promethist/core/dialogue/BasicEnglishDialogue.kt
+++ services/core/lib/src/main/kotlin/com/promethist/core/dialogue/BasicEnglishDialogue.kt
@@ -1,52 +1,39 @@
 package com.promethist.core.dialogue
 
+import com.promethist.core.model.DialogueEvent
+import java.util.*
+
 abstract class BasicEnglishDialogue() : BasicDialogue() {
 
     var basicId = 1
-    val basicVersionGlobalIntent = GlobalIntent(basicId++, "basicVolumeUpGlobalIntent", "version")
-    val basicVersionSubDialogue = SubDialogue(basicId++, "\$basic/version/1") {
-//        object : Dialogue() {
-//            override val name: String = "version"
-//            val response = Response(nextId--, { "\$version, dialogue $name" })
-//
-//            init {
-//                start.next = response
-//                response.next = stop
-//            }
-//        }
-        mapOf()
-    }
-    val basicVolumeUpGlobalIntent = GlobalIntent(basicId++, "basicVolumeUpGlobalIntent", "volume up", "louder")
-    val basicVolumeUpSubDialogue = SubDialogue(basicId++, "\$basic/volumeUp/1") {
-//        object : Dialogue() {
-//            override val name: String = "volume_up"
-//            val response = Response(nextId--, { "\$volume_up setting volume up" })
-//            init {
-//                start.next = response
-//                response.next = stop
-//            }
-//        }
-        mapOf()
-    }
-    val basicVolumeDownGlobalIntent = GlobalIntent(basicId++, "basicVolumeDownGlobalIntent", "volume down", "quieter")
-    val basicVolumeDownSubDialogue = SubDialogue(basicId++, "\$basic/volumeDown/1") {
-//        object : Dialogue() {
-//            override val name: String = "volume_down"
-//            val response = Response(nextId--, { "\$volume_up setting volume down" })
-//            init {
-//                start.next = response
-//                response.next = stop
-//            }
-//        }
-        mapOf()
+    //Nodes
+    val _goBack = GoBack(basicId++)
+    val _basicVersionGlobalIntent = GlobalIntent(basicId++, "basicVolumeUpGlobalIntent", "version")
+    val _basicVersionResponse = Response(basicId++, { "\$version, dialogue $dialogueName" })
+
+    val _basicVolumeUpGlobalIntent = GlobalIntent(basicId++, "basicVolumeUpGlobalIntent", "volume up", "louder")
+    val _basicVolumeUpResponse = Response(basicId++, { "\$volume_up setting volume up" })
+
+    val _basicVolumeDownGlobalIntent = GlobalIntent(basicId++, "basicVolumeDownGlobalIntent", "volume down", "quieter")
+    val _basicVolumeDownResponse = Response(basicId++, { "\$volume_down setting volume down" })
+
+    val _basicLogApplicationErrorGlobalIntent = GlobalIntent(basicId++, "basicLogApplicationErrorGlobalIntent", "application error", "application problem")
+    val _basicLogApplicationErrorResponse1 = Response(basicId++, {"What is the problem?"})
+    val _basicLogApplicationErrorResponse2 = Response(basicId++, {"Thank you for the report. Let's get back now"})
+    val _basicLogApplicationErrorUserInputTransition = Transition(_basicLogApplicationErrorResponse2)
+    val _basicLogApplicationErrorUserInput = UserInput(basicId++, arrayOf()) {
+        dialogueEvent = DialogueEvent(datetime = Date(), type = DialogueEvent.Type.userError, userId = user._id, sessionId = session._id, applicationName = application.name, dialogueName = application.dialogueName, nodeId = turn.endFrame?.nodeId, text = input.transcript.text)
+        _basicLogApplicationErrorUserInputTransition
     }
 
+
+
+    val _basicLogApplicationCommentGlobalIntent = GlobalIntent(basicId++, "basicLogApplicationCommentGlobalIntent", "application comment", "application command")
+
+
     init {
-        basicVersionGlobalIntent.next = basicVersionSubDialogue
-        basicVolumeUpGlobalIntent.next = basicVolumeUpSubDialogue
-        basicVolumeDownGlobalIntent.next = basicVolumeDownSubDialogue
-        basicVersionSubDialogue.next = repeat
-        basicVolumeUpSubDialogue.next = repeat
-        basicVolumeDownSubDialogue.next = repeat
+        _basicLogApplicationErrorGlobalIntent.next = _basicLogApplicationErrorResponse1
+        _basicLogApplicationErrorResponse1.next = _basicLogApplicationErrorUserInput
+        _basicLogApplicationErrorResponse2.next = _goBack
     }
 }
