737c3d9acc279231642f07ee10f288b492b4b841
konrajak <konradjakub@gmail.com>
2020-06-29 13:31:46 +0200

    Added global intents

--- services/core/lib/src/main/kotlin/com/promethist/core/dialogue/BasicEnglishDialogue.kt
+++ services/core/lib/src/main/kotlin/com/promethist/core/dialogue/BasicEnglishDialogue.kt
@@ -7,7 +7,7 @@ abstract class BasicEnglishDialogue() : BasicDialogue() {
 
     var basicId = 1
     //Nodes
-    val _goBack = GoBack(basicId++)
+    val _goBack = GoBack(basicId++, repeat = true)
     val _basicVersionGlobalIntent = GlobalIntent(basicId++, "basicVolumeUpGlobalIntent", "version")
     val _basicVersionResponse = Response(basicId++, { "\$version, dialogue $dialogueName" })
 
@@ -22,18 +22,37 @@ abstract class BasicEnglishDialogue() : BasicDialogue() {
     val _basicLogApplicationErrorResponse2 = Response(basicId++, {"Thank you for the report. Let's get back now"})
     val _basicLogApplicationErrorUserInputTransition = Transition(_basicLogApplicationErrorResponse2)
     val _basicLogApplicationErrorUserInput = UserInput(basicId++, arrayOf()) {
+        val transition = Transition(_basicLogApplicationErrorResponse2)
         dialogueEvent = DialogueEvent(datetime = Date(), type = DialogueEvent.Type.userError, userId = user._id, sessionId = session._id, applicationName = application.name, dialogueName = application.dialogueName, nodeId = turn.endFrame?.nodeId, text = input.transcript.text)
-        _basicLogApplicationErrorUserInputTransition
+        transition
     }
 
-
-
-    val _basicLogApplicationCommentGlobalIntent = GlobalIntent(basicId++, "basicLogApplicationCommentGlobalIntent", "application comment", "application command")
-
+    val _basicLogApplicationCommentGlobalIntent = GlobalIntent(basicId++, "basicLogApplicationCommentGlobalIntent", "application command", "application comment")
+    val _basicLogApplicationCommentResponse1 = Response(basicId++, {"What would you like to comment?"})
+    val _basicLogApplicationCommentResponse2 = Response(basicId++, {"Thank you for the report. Let's get back now"})
+    val _basicLogApplicationCommentUserInputTransition = Transition(_basicLogApplicationCommentResponse2)
+    val _basicLogApplicationCommentUserInput = UserInput(basicId++, arrayOf()) {
+        val transition = Transition(_basicLogApplicationCommentResponse2)
+        dialogueEvent = DialogueEvent(datetime = Date(), type = DialogueEvent.Type.userComment, userId = user._id, sessionId = session._id, applicationName = application.name, dialogueName = application.dialogueName, nodeId = turn.endFrame?.nodeId, text = input.transcript.text)
+        transition
+    }
 
     init {
+        _basicVersionGlobalIntent.next = _basicVersionResponse
+        _basicVersionResponse.next = _goBack
+
+        _basicVolumeUpGlobalIntent.next = _basicVolumeUpResponse
+        _basicVolumeUpResponse.next = _goBack
+
+        _basicVolumeDownGlobalIntent.next = _basicVolumeDownResponse
+        _basicVolumeDownResponse.next = _goBack
+
         _basicLogApplicationErrorGlobalIntent.next = _basicLogApplicationErrorResponse1
         _basicLogApplicationErrorResponse1.next = _basicLogApplicationErrorUserInput
         _basicLogApplicationErrorResponse2.next = _goBack
+
+        _basicLogApplicationCommentGlobalIntent.next = _basicLogApplicationCommentResponse1
+        _basicLogApplicationCommentResponse1.next = _basicLogApplicationCommentUserInput
+        _basicLogApplicationCommentResponse2.next = _goBack
     }
 }
