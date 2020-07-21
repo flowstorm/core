0bc13f30220e1cbf28b4b1595c9ca8a6f832aa17
Tomáš Zajíček <tomas.zajicek@promethist.ai>
2020-07-21 18:27:00 +0000

    unique API object instance per class + dialogue model

--- services/core/lib/src/main/kotlin/com/promethist/core/runtime/DialogueApi.kt
+++ services/core/lib/src/main/kotlin/com/promethist/core/runtime/DialogueApi.kt
@@ -10,7 +10,7 @@ open class DialogueApi(val dialogue: BasicDialogue) : Api() {
         val instances = mutableMapOf<String, DialogueApi>()
 
         inline fun <reified T : DialogueApi> get(dialogue: BasicDialogue): T =
-                instances.getOrPut(T::class.java.canonicalName) {
+                instances.getOrPut(T::class.java.canonicalName + "@" + dialogue.dialogueName) {
                     T::class.primaryConstructor!!.call(dialogue)
                 } as T
     }
