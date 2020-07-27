cfe33e6fd19afabe4365193c37703c47b370cae4
Tomas Kormanak <tomas.kormanak@kormik.net>
2020-07-27 15:18:18 +0200

    PAI-286: Process global action also from parent dialogues.

--- services/core/lib/src/main/kotlin/com/promethist/core/runtime/DialogueManager.kt
+++ services/core/lib/src/main/kotlin/com/promethist/core/runtime/DialogueManager.kt
@@ -69,10 +69,17 @@ class DialogueManager : Component {
         val dialogue = dialogueFactory.get(frame)
 
         val commands = node.commands + dialogue.globalCommands
+        commands.firstOrNull { it.command == context.input.command }?.let {
+            return frame.copy(nodeId = it.id)
+        }
 
-        return commands.firstOrNull { it.command == context.input.command }?.let {
-            frame.copy(nodeId = it.id)
-        } ?: error("Action ${context.input.command} not found in dialogue.")
+        context.session.dialogueStack.distinctBy { it.name }.reversed().forEach { f ->
+            dialogueFactory.get(f).globalCommands.firstOrNull { it.command == context.input.command }?.let {
+                return f.copy(nodeId = it.id)
+            }
+        }
+
+        error("Action ${context.input.command} not found in dialogue.")
     }
 
     private fun getNode(frame: Frame, context: Context): Dialogue.Node =
