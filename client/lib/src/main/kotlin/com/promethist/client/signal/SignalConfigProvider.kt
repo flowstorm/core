52d0878ce8d0b9ac4982cce557757160dc37d7fb
Tomas Kormanak <tomas.kormanak@kormik.net>
2020-08-30 22:39:13 +0200

    Fix wrong merge.

--- client/lib/src/main/kotlin/com/promethist/client/signal/SignalConfigProvider.kt
+++ client/lib/src/main/kotlin/com/promethist/client/signal/SignalConfigProvider.kt
@@ -1,5 +1,5 @@
 package com.promethist.client.signal
 
-abstract class SignalConfigProvider(val name: String, val format: Format, val enabled: Boolean) : SignalProvider() {
+abstract class SignalConfigProvider(val name: String, val format: Format, val enabled: Boolean) : SignalProvider {
     enum class Format { Value, Properties, JSON }
 }
