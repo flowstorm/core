f76883bfbb9873116d643fa77ce0767ca46484e0
Tomáš Zajíček <tomas.zajicek@promethist.ai>
2020-09-07 13:25:21 +0200

    FIX Editor role enough for calling dialogue resource; signal file sequence starts with 0

--- client/lib/src/main/kotlin/com/promethist/client/signal/SignalFileProvider.kt
+++ client/lib/src/main/kotlin/com/promethist/client/signal/SignalFileProvider.kt
@@ -5,7 +5,7 @@ import java.io.FileInputStream
 class SignalFileProvider(name: String, format: Format, val file: String, enabled: Boolean = true, sleep: Long = 1000, continuous: Boolean = false) :
         SignalConfigurableProvider(name, format, enabled, sleep, continuous) {
 
-    private var sequenceId = 1
+    private var sequenceId = 0
 
     override fun load() = file.replace("##", sequenceId++.toString().padStart(2, '0')).let {
         logger.debug("loading signal data from file $it")
