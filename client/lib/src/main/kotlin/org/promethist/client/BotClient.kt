a363ed7fdb8b8f216e71c11d87649f43b9fa4d92
Jan Pichl <honza.pichl@gmail.com>
2021-01-29 18:06:13 +0100

    Reverted accidental commit

--- client/lib/src/main/kotlin/org/promethist/client/BotClient.kt
+++ client/lib/src/main/kotlin/org/promethist/client/BotClient.kt
@@ -350,7 +350,7 @@ class BotClient(
     fun doText(text: String, attributes: PropertyMap = emptyMap()) {
         if (context.sessionId == null || (sleepLimitTime > 0 && sleepLimitTime < System.currentTimeMillis()))
             context.sessionId = UUID.randomUUID().toString()
-        if (socket.state == BotSocket.State.Open) {
+        if (socket.state == BotSocket.State.Open && (state == State.Sleeping || state == State.Listening)) {
             val request = Request(
                     context.key,
                     context.deviceId,
