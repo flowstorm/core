c2e53dfb02c6b5f840204cd05fc14cf792071de8
Tomas Kormanak <tomas.kormanak@kormik.net>
2021-03-16 13:52:57 +0100

    CORE-33: Allow default value for ConfigValue, throw exception when key is missing.

--- common/app/src/main/kotlin/ai/flowstorm/common/config/ConfigValue.kt
+++ common/app/src/main/kotlin/ai/flowstorm/common/config/ConfigValue.kt
@@ -2,4 +2,10 @@ package ai.flowstorm.common.config
 
 @Target(AnnotationTarget.FIELD, AnnotationTarget.VALUE_PARAMETER)
 @Retention(AnnotationRetention.RUNTIME)
-annotation class ConfigValue(val key: String)
+annotation class ConfigValue(val key: String, val default: String = NULL) {
+    companion object {
+
+        //Workaround: Annotation parameter can't be null
+        const val NULL = "___NULL___"
+    }
+}
