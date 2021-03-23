33e94ae602f58f3f208d4f96dad9f59d0ef08db6
Tomáš Zajíček <tomas.zajicek@promethist.ai>
2021-03-23 15:55:45 +0100

    App 50 configurable sentry dsn (#20)
    * update
    
    * update

--- common/app/src/main/kotlin/ai/flowstorm/common/monitoring/SentryMonitor.kt
+++ common/app/src/main/kotlin/ai/flowstorm/common/monitoring/SentryMonitor.kt
@@ -1,25 +1,27 @@
 package ai.flowstorm.common.monitoring
 
+import ai.flowstorm.common.config.ConfigValue
 import io.sentry.Sentry
 import io.sentry.SentryEvent
+import io.sentry.SentryOptions
 
-class SentryMonitor : AbstractMonitor() {
+class SentryMonitor(@ConfigValue("sentry.dsn") val dsn: String) : AbstractMonitor() {
 
-    private var initialized = false
+    init {
+        val options = SentryOptions()
+        options.dsn = dsn
+        Sentry.init(options)
+    }
 
     override fun capture(e: Throwable, extras: Map<String, Any?>) {
         try {
-            if (!initialized) {
-                initialized = true
-                Sentry.init()
-            }
             with(SentryEvent()) {
                 throwable = e
                 setExtras(extras)
                 Sentry.captureEvent(this)
             }
         } catch (t: Throwable) {
-            logger.error("Monitoring error ${t.message}", t)
+            logger.error("Sentry monitor error: ${t.message}", t)
         }
     }
 }
